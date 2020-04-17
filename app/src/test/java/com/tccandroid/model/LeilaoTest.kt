81046586f9630d88ef9f5469bb08c2603936683f
Gustavo <pereiragustavo215@gmail.com>
2020-04-17 12:02:50 -0300

    mais teste e renomeando para o padrao nome,estado do teste, resultado esperado

--- app/src/test/java/com/tccandroid/model/LeilaoTest.kt
+++ app/src/test/java/com/tccandroid/model/LeilaoTest.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 class LeilaoTest {
 
     @Test
-    fun getDescricao() {
+    fun getDescricaoQuandoRecebeDescricaoDevolverDescricao() {
         //criar cenário de teste
         var console = Leilao("Console")
 
@@ -18,12 +18,34 @@ class LeilaoTest {
     }
 
     @Test
-    fun getMaiorPreco() {
+    fun getMaiorLanceQuandoRecebeApenasUmLanceDevolveMaiorLance() {
         val console = Leilao("Console")
         console.propoe(Lance(Usuario("Gustavo"), 200.00))
 
         val maiorLanceDevolvido = console.maiorLance
 
         assertEquals(200.00, maiorLanceDevolvido, 0.0001)
+    }
+
+    @Test
+    fun getMaiorLanceQuandoRecebeMaisDeUmLanceEmOrdemCrescenteDevolveMaiorLance() {
+        val computador = Leilao("Computador")
+        computador.propoe(Lance(Usuario("Gustavo"), 1000.00))
+        computador.propoe(Lance(Usuario("Maria"), 2000.00))
+
+        val maiorValorDevolvido = computador.maiorLance
+
+        assertEquals(2000.00, maiorValorDevolvido, 0.0001)
+    }
+
+    @Test
+    fun getMaiorLanceQuandoRecebeMaisDeUmLanceEmOrdemDecrescenteDevolveMaiorLance() {
+        val computador = Leilao("Computador")
+        computador.propoe(Lance(Usuario("Maria"), 2000.00))
+        computador.propoe(Lance(Usuario("Gustavo"), 1000.00))
+
+        val maiorValorDevolvido = computador.maiorLance
+
+        assertEquals(2000.00, maiorValorDevolvido, 0.0001)
     }
 }
