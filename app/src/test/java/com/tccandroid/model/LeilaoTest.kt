f6ac3584403f2691d8e2de1a64474eb482c7e963
Gustavo <pereiragustavo215@gmail.com>
2020-04-17 21:26:42 -0300

    Criando teste parar o menor lance

--- app/src/test/java/com/tccandroid/model/LeilaoTest.kt
+++ app/src/test/java/com/tccandroid/model/LeilaoTest.kt
@@ -48,4 +48,38 @@ class LeilaoTest {
 
         assertEquals(2000.00, maiorValorDevolvido, 0.0001)
     }
+
+    @Test
+    fun deve_DevolveMenorLance_QuandoRecebeApenasUmLance() {
+        val console = Leilao("Console")
+        console.propoe(Lance(Usuario("Gustavo"), 200.00))
+
+        val menorLanceDevolvido = console.menorLance
+
+        assertEquals(200.00, menorLanceDevolvido, 0.0001)
+    }
+
+    @Test
+    fun deve_DevolveMenorLance_QuandoRecebeMaisDeUmLanceEmOrdemCrescente() {
+        val computador = Leilao("Computador")
+        computador.propoe(Lance(Usuario("Gustavo"), 1000.00))
+        computador.propoe(Lance(Usuario("Maria"), 2000.00))
+
+        val menorValorDevolvido = computador.menorLance
+
+        assertEquals(2000.00, menorValorDevolvido, 0.0001)
+    }
+
+
+    @Test
+    fun deve_DevolveMenorLance_QuandoRecebeMaisDeUmLanceEmOrdemDecrescente() {
+        val computador = Leilao("Computador")
+        computador.propoe(Lance(Usuario("Gustavo"), 2000.00))
+        computador.propoe(Lance(Usuario("Maria"), 1000.00))
+
+
+        val menorLanceDevolvido = computador.menorLance
+
+        assertEquals(1000.00, menorLanceDevolvido, 0.0001)
+    }
 }
